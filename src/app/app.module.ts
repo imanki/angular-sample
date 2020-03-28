91fb46139aae7b678d3be6e326669e4c97862e53
imanki <akaushik198@gmail.com>
2020-03-28 11:40:12 +0530

    candidate-module-created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { CandidateModule } from './candidate/candidate.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule,CandidateModule],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
---
+++ src/app/candidate/modal/candidate.modal.ts
@@ -0,0 +1,9 @@
+import { GenderEnum } from "./gender.enum";
+
+export class Candidate{
+  first_name:string;
+  last_name:string;
+  email:string;
+  gender:GenderEnum;
+
+}
